a101cf11d12aa94a7b3a482bc7bf083043ee9d9d
kirstenbryan <kirstebs@miamioh.edu>
2017-04-07 14:34:49 -0400

    Update houseRobber.java

--- Java/Airbnb/houseRobber.java
+++ Java/Airbnb/houseRobber.java
@@ -8,8 +8,12 @@
 public class Solution {
 
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
+        if(nums.length == 0) {
+            return 0;
+        }
+        if(nums.length == 1) {
+            return nums[0];
+        }
         
         int[] dp = new int[nums.length]; 
         dp[0] = nums[0];
