ba123badc5dcd77d14fb8bbc534062cfd62566ba
kirstenbryan <kirstebs@miamioh.edu>
2017-04-07 14:27:34 -0400

    Update convertSortedArrayToBinarySearchTree.java

--- Java/Airbnb/convertSortedArrayToBinarySearchTree.java
+++ Java/Airbnb/convertSortedArrayToBinarySearchTree.java
@@ -12,20 +12,33 @@
 public class Solution {
 
     public TreeNode sortedArrayToBST(int[] nums) {
+        
         if(nums.length == 0) return null;
+        
         TreeNode root = helper(nums, 0, nums.length - 1);
-        return root;      
+        
+        return root;
+        
     }
     
-    private TreeNode helper(int[] nums, int start, int end) {     
+    private TreeNode helper(int[] nums, int start, int end) {    
+        
         if(start <= end) { 
+            
             int mid = (start + end) / 2; 
+            
             TreeNode current = new TreeNode(nums[mid]);    
+            
             current.left = helper(nums, start, mid - 1);
+            
             current.right = helper(nums, mid + 1, end); 
-            return current;         
+            
+            return current;     
+            
         } 
-        return null;       
+        
+        return null;  
+        
     }
     
 }
